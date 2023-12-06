418f6d8d13a3c2a86658a00685bda09802681735
Mahendhar <7093741662mahiv@gmail.com>
2023-12-06 11:47:36 +0530

    added some more data

--- mysql.sql
+++ mysql.sql
@@ -1,4 +1,8 @@
 --CREATE TABLE product(id INT, title TEXT, price FLOAT, descriptions TEXT, category varchar(50), images TEXT, sold BOOLEAN, date_of_sale TIMESTAMP);
 
 
+--SELECT * FROM product;
+
+
+
 SELECT * FROM product;
